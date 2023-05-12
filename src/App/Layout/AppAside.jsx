fe4894373e75ba6deaaad35152cdf1005b6333e5
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-05-12 16:52:41 +0200

    Update

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,4 +1,3 @@
-
 import { NavLink } from 'react-router-dom';
 import { HouseIcon } from '../Components/Icons/HouseIcon';
 import { ElementIcon } from '../Components/Icons/ElementIcon';
@@ -19,28 +18,34 @@ export function AppAside() {
               <HouseIcon className="menu-icon" /> Dashboard
             </NavLink>
           </li>
+
           <li>
             <NavLink className="aside-row" to="my-cv">
               <PersonalCardIcon className="menu-icon" /> Moje CV
             </NavLink>
           </li>
+
           <li>
             <NavLink className="aside-row" to="blocks">
               <ElementIcon className="menu-icon" /> Bloki
             </NavLink>
           </li>
+
           <li>
             <NavLink className="aside-row" to="exercises">
               <EditIcon className="menu-icon" /> Ćwiczenia
             </NavLink>
           </li>
+
           <li>
             <NavLink className="aside-row" to="blog">
               <CalendarIcon className="menu-icon" /> Blog
-           <li>
+            </NavLink>
+          </li>
+
+          <li>
             <NavLink className="aside-row" to="faq">
               <Faq className="menu-icon" /> FAQ
-          </li>
             </NavLink>
           </li>
         </ul>
@@ -49,4 +54,3 @@ export function AppAside() {
     </aside>
   );
 }
-
