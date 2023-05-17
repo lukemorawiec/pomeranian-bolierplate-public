10518bf358451b3a87de42d8d3e3231027ccdaa1
Luke Morawiec <luke.morawiec@gmail.com>
2023-05-17 21:23:58 +0200

    Circle area exercise

--- src/App/Exercises/Js/Boolean/index.jsx
+++ src/App/Exercises/Js/Boolean/index.jsx
@@ -1,39 +1,68 @@
 import './styles.css';
+import { useState } from 'react';
+
+const MESSAGES = {
+  moreThanTen: 'Given number is too big',
+  lessThanOne: 'Given number is too small',
+  notEven: 'Given number is not an even number',
+};
 
 export function Booleans() {
-  const value1 = 8;
-  // const value2 = 2;
-  // const value3 = 3;
+  const [value, setValue] = useState();
+  const parsedValue = Number(value);
 
-  let result = false;
+  const calculateCircleArea = () => {
+    const calculation = Math.PI * Math.pow(parsedValue, 2);
+    return Math.round(calculation);
+  };
 
-  // if (value1 === 1) {
-  //   result = 'jeden';
-  // } else if (value1 === 2) {
-  //   result = 'dwa';
-  // } else if (value1 === 3) {
-  //   result = 'trzy';
-  // } else {
-  //   result = 'inna cyfra';
-  // }
+  const getErrorMessage = () => {
+    if (!isWithinLimits) {
+      return biggerThenOne ? MESSAGES['moreThanTen'] : MESSAGES['lessThanOne'];
+    }
 
-  // switch (value1) {
-  //   case 1:
-  //     result = 'jeden';
-  //     break;
-  //   case 2:
-  //     result = 'dwa';
-  //     break;
-  //   case 3:
-  //     result = 'trzy';
-  //     break;
-  //   default:
-  //     result = 'inna cyfra';
-  //     break;
-  // }
+    if (!isEven) {
+      return MESSAGES['notEven'];
+    }
+  };
 
-  result = value1 >= 1 && value1 <= 3 ? 'mieści się w zakresie' : 'inna liczba';
+  const biggerThenOne = parsedValue >= 1;
+  const smallerThanTen = parsedValue <= 10;
+  const isWithinLimits = biggerThenOne && smallerThanTen;
+  const isEven = parsedValue % 2 === 0;
 
-  console.log(result);
-  return <div>Operatory logiczne</div>;
+  const isNumberReadyToCalculate = isWithinLimits && isEven;
+
+  const isNumberGiven = !!value;
+
+  const showResult = () => {
+    return (
+      <div>
+        {isNumberReadyToCalculate ? (
+          <h3>
+            Circle area equals: {calculateCircleArea()}m<sup>2</sup>
+          </h3>
+        ) : (
+          <div>
+            <h4>Something is wrong!</h4>
+            {getErrorMessage()}
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  return (
+    <div className="boolean-exercises-container">
+      <input
+        type="number"
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
+      meters
+      {isNumberGiven ? showResult() : <h4>Give me a number :)</h4>}
+    </div>
+  );
 }
