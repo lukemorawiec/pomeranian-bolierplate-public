e9d94ec477450005f9f56bd7e8d160aa07d62c0b
Karina Złotogórska-Krzemińska <karina.zlotogorska@pomeranianstartit.pl>
2023-05-17 18:18:43 +0200

    Create Boolean3

--- src/App/Exercises/Js/Boolean/Boolean.jsx
+++ src/App/Exercises/Js/Boolean/Boolean.jsx
@@ -1,20 +1,7 @@
 import './styles.css';
 
-console.log(result);
+console.log(typeof 2.5);
 
 export function Boolean() {
-  const p = true;
-  const q = false;
-
-  const condition1 = !(p && q);
-  const condition2 = !p || !q;
-
-  console.log(condition1 === condition2);
-
-  return (
-    <div>
-      Operatory logiczne
-      {condition1 && <h1> TAJNY TEKST</h1>}
-    </div>
-  );
+  return <div>Operatory logiczne</div>;
 }
