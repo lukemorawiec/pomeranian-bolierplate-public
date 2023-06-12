b3ea36d622250aecf22e0cef97cbd03e89fa1f3b
Kamil Grygiel <kamil@hausegger-inc.com>
2023-06-12 18:36:16 +0200

    Create component with useEffects

--- src/App/Exercises/ReactExercises/SideEffects/SideEffects.jsx
+++ src/App/Exercises/ReactExercises/SideEffects/SideEffects.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 export function SideEffects() {
-  return <div className="side-effects">SideEffects</div>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('value: ' + value);
+
+    return () => {
+      console.log('return useeffect value');
+    };
+  }, [value]);
+
+  const [age, setAge] = useState(10);
+
+  useEffect(() => {
+    console.log(`age: ${age}`);
+
+    return () => {
+      console.log('return useeffect age');
+    };
+  }, [age]);
+
+  return (
+    <div className="side-effects">
+      <button onClick={() => setValue(value + 1)}>Set Value</button>
+      {value}
+      <br />
+      <br />
+      <button onClick={() => setAge(age + 1)}>Set Age</button>
+      {age}
+    </div>
+  );
 }
