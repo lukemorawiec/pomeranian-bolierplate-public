45f438f3a68f8d039aa2faf4b1d8c00ea05c1753
Luke Morawiec <luke.morawiec@gmail.com>
2023-06-22 08:13:48 +0200

    UseRef exercises

---
+++ src/App/Exercises/ReactExercises/UseRef/router-data.jsx
@@ -0,0 +1,10 @@
+import { UseRef } from './UseRef';
+
+export const useRefMetaData = {
+  path: 'use-ref',
+  date: '21-06-2023',
+  linkLabel: 'UseRef',
+  blockNo: 123,
+  element: <UseRef />,
+  tags: ['useRef', 'react', 'hook', 'javascript'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { SideEffectsData } from './SideEffects/router-data';
 import { routerData as TodoListRouterData } from './TodoList/router-data';
+import { useRefMetaData } from './UseRef/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   SideEffectsData,
   TodoListRouterData,
+  useRefMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
