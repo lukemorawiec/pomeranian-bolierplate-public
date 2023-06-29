d52c6e045b1d44c689343b380eca9a9900422de4
Kamil Grygiel <kamil@hausegger-inc.com>
2023-06-29 20:04:06 +0200

    add husky

--- src/App/Exercises/Categories.jsx
+++ src/App/Exercises/Categories.jsx
@@ -6,8 +6,35 @@ import './categories.css';
 export const Categories = () => {
   return (
     <>
+      <div>
+        <h1>TEST</h1>
+        <p>TEKST</p>
+      </div>
+
+      <div>
+        <h2>TEST2</h2>
+        <p>test</p>
+      </div>
+
+      <div>
+        <p>1</p>
+        <p>1</p>
+      </div>
+
+      <div className="test">
+        <h1>TEST</h1>
+        <div>
+          <p>aasadad</p>
+        </div>
+      </div>
+
+      <div>
+        <h3>TEST</h3>
+        <p>DEscription</p>
+      </div>
+
       <div className="exercises-categories">
-        <h2>Kategorie</h2>
+        <h2>Kategorie cos tam</h2>
 
         <NavLink to="html-css">HTML & CSS - lista ćwiczeń</NavLink>
         <NavLink to="js">JS - lista ćwiczeń</NavLink>
